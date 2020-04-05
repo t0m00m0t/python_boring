b9fc0017e2cbaa5498da7e7fc5f2e6d7ab146a18
t0m00m0t <Tomoo.Koyano@gmail.com>
2020-04-05 16:19:25 +0900

    Create Chap8.10.2 Problem

---
+++ textGen.py
@@ -0,0 +1,28 @@
+#! python 3
+# textGen.py - テキストファイルを読み込み、特定のキーワードを書き換える
+
+import os
+
+keywords = ['ADJECTIVE':'an', 'NOUN':'a', 'ADVERB':'an', 'VERB':'a']
+
+cwd_dir = os.getcwd()
+for filedir in os.listdir(cwd_dir):
+    if filedir.endswith('.txt'):
+        text_file = open(filedir)
+        repl_filename = filedir.split('.')[0] + '_repl.txt'
+        repl_file = open(repl_filename, 'w')
+        repl_lines = []
+        for text_line in text_file.readlines():
+            print('Target line ....')
+            print(text_line)
+            words = text_line.split()
+            for i in range(len(words)):
+                if words[i] in keywords.keys():
+                    print('Enter {0} {1}:'.format(keywords[words[i]], words[i]))
+                    words[i] = input()
+            repl_line = ' '.join(words)
+            print('Replaced line ....')
+            print(repl_line)
+            repl_file.write(repl_line)
+        text_file.close()
+        repl_file.close()
